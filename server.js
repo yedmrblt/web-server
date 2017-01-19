7f9b0f0fee3c78a313cb286e3c57df2502927e3f
Yunus Emre Demirbulut <y.emre.demirbulut@gmail.com>
2017-01-20 01:17:14 +0300

    Update language

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var middleware = {
 
 app.use(middleware.logger);
 
-app.get('/about', function (req, res) {
+app.get('/about', middleware.requireAuthentication, function (req, res) {
   res.send('About Us!');
 });
 
